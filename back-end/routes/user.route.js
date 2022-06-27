3cc89047c27afb6a5c44b5e2100841bdb866d341
Rak <hfr@esokia.com>
2022-06-27 15:05:06 +0300

    create route delete user

--- back-end/routes/user.route.js
+++ back-end/routes/user.route.js
@@ -1,9 +1,14 @@
 const express = require('express');
-const { addUser, getUser } = require('../controllers/user.controller');
+const {
+  addUser,
+  getUser,
+  deleteUser,
+} = require('../controllers/user.controller');
 
 const router = express.Router();
 
 router.post('/add-user', addUser);
 router.get('/list-user', getUser);
+router.delete('/delete/:id', deleteUser);
 
 module.exports = { routes: router };
