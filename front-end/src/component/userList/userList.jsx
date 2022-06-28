0d600b79fb2a36df4443dbc662b94c5435a27c90
Rak <hfr@esokia.com>
2022-06-28 16:21:30 +0300

    fix show hidden modal

--- front-end/src/component/userList/userList.jsx
+++ front-end/src/component/userList/userList.jsx
@@ -1,19 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import User from './user';
+import { Button } from 'react-bootstrap';
+import Modals from '../modal/modal';
 
 const UserList = () => {
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>email</th>
-          <th>poste</th>
-          <th>Action</th>
-        </tr>
-      </thead>
-      <User />
-    </table>
+    <>
+      <Button variant='primary' onClick={handleShow}>
+        Launch demo modal
+      </Button>
+      <Modals
+        handleClose={handleClose}
+        show={show}
+        handleShow={handleShow}
+        setShow={setShow}
+      />
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>email</th>
+            <th>poste</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <User />
+      </table>
+    </>
   );
 };
 
