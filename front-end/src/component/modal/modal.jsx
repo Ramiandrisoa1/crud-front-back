3168372e1ada95f07b8962e656d5a091a798b512
Rak <hfr@esokia.com>
2022-06-29 09:32:44 +0300

    feat useContext

--- front-end/src/component/modal/modal.jsx
+++ front-end/src/component/modal/modal.jsx
@@ -1,19 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Modal, Button } from 'react-bootstrap';
+import { ShowContext } from '../userList/userList';
 
-function Modals({ show, handleClose }) {
+function Modals() {
+  const value = useContext(ShowContext);
   return (
     <>
-      <Modal show={show} onHide={handleClose}>
+      <Modal show={value.show} onHide={value.handleClose}>
         <Modal.Header closeButton>
           <Modal.Title>Modal heading</Modal.Title>
         </Modal.Header>
         <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
         <Modal.Footer>
-          <Button variant='secondary' onClick={handleClose}>
+          <Button variant='secondary' onClick={value.handleClose}>
             Close
           </Button>
-          <Button variant='primary' onClick={handleClose}>
+          <Button variant='primary' onClick={value.handleClose}>
             Save Changes
           </Button>
         </Modal.Footer>
